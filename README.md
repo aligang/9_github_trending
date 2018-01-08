6b3999b2dabaaaa726a2b1c5c0576aaca6d9717e
aligang <aligang@yandex.ru>
2018-01-08 17:37:04 +0000

    задание выполнено

--- README.md
+++ README.md
@@ -1,7 +1,78 @@
-# Github Trends
+## 1.Что это такое ?
 
-[TODO. There will be project description]
+Этот код позволяет сформировать и отобразить список из 20 самых популярных github репозиториев, созданных за полседние 7 дней
 
-# Project Goals
+## 2.Системные требования
+Для работы с программой понадобится Python3.5 (который скорее всего у вас уже установлен, если Вы используете Linux)  
+Также может понадобиться установить модуль `requests`, сделать это можно выполнив `pip3 install -r requirements.txt`
+```
+# pip3 install -r requirements.txt
+```
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+## 3.Где можно скачать  
+Можно форкнуть здесь - [20 свежих и  популярных репозиториев](https://github.com/aligang/9_github_trending)  
+и затем скачать 
+```
+git clone https://github.com/<юзернейм-аккаунта-на-гите>/9_github_trending
+```
+
+## 4.Как этим пользоваться...  
+*a.Данный код может быть исползован как самостоятельная программа,*  
+
+```bash
+$   python3 github_trending.py 
+
+Список 20  самых популярных репозиториев открытыза последние 7 дней
+
+--------------------------------------------------------------
+
+Репозиторий: paragonie/past,
+расположен https://api.github.com/repos/paragonie/past
+
+открытые issues:
+issue: setClaims overwrites what was defined with setExpiration in the php example
+описан https://api.github.com/repos/paragonie/past/issues/26
+
+--------------------------------------------------------------
+
+Репозиторий: burningcl/wechat_jump_hack,
+расположен https://api.github.com/repos/burningcl/wechat_jump_hack
+
+открытые issues:
+issue: 运行不成功
+описан https://api.github.com/repos/burningcl/wechat_jump_hack/issues/61
+issue: 分最高的是用的那款手机啊？
+описан https://api.github.com/repos/burningcl/wechat_jump_hack/issues/60
+issue: 为什么我的手机向左跳的时候可以 但是向右跳的时候不行 biu的一下就飞了
+описан https://api.github.com/repos/burningcl/wechat_jump_hack/issues/59
+issue: 系统找不到指定的文件
+описан https://api.github.com/repos/burningcl/wechat_jump_hack/issues/58
+
+--------------------------------------------------------------
+
+Репозиторий: turbo/KPTI-PoC-Collection,
+расположен https://api.github.com/repos/turbo/KPTI-PoC-Collection
+
+--------------------------------------------------------------
+
+```
+
+## 5.Какие функции могут быть переиспользованы в вашем коде
+Функция `calculate_reference_date(days)` выдает дату, которая соответсвует дню, который был `days` суток назад
+Функция `get_trending_repositories` формирует выдаёт список самых 20 популярных репозиториев созданных за последнюю неделю
+Функция `get_open_issues`  обогащает список, полученный от функции `get_trending_repositories` информацией об активных issues
+
+Импортировать и использовать функцию коди можно  следующим образом:  
+```python
+from github_trending import calculate_reference_date
+from github_trending import get_trending_repositories
+from github_trending import get_open_issues
+
+
+reference_date = calculate_reference_date(days)
+trending_repos_list = get_trending_repositories(top_size, reference_date)
+trending_repos_info_list = get_open_issues(trending_repos_list)
+```
+
+## 6. Цели
+Код создан в учебных целях. В рамках учебного курса по веб-разработке ― [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,102 @@
+#!/usr/bin/env python3
 
 
-def get_trending_repositories(top_size):
-    pass
+import requests
+import datetime
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def calculate_reference_date(days):
+    today_date = datetime.date.today()
+    timeshift = datetime.timedelta(days=days)
+    reference_date = today_date - timeshift
+    return reference_date
+
+
+def get_trending_repositories(top_size, reference_date):
+    headers = {"Accept": "Application/JSON"}
+    uri = (
+        "https://api.github.com/search/repositories?q=created:>={}"
+        "&sort=star&page=1&per_page={}".format(
+            reference_date,
+            top_size
+        )
+    )
+    trending_repos_responce = requests.get(uri, headers=headers)
+    trending_repos_list = trending_repos_responce.json()["items"]
+    return trending_repos_list
+
+
+def get_open_issues(trending_repos_list):
+    trending_repos_info_list = [
+        {"name": trending_repo["full_name"], "url": trending_repo["url"]}
+        for trending_repo in trending_repos_list
+    ]
+    trending_repos_names = [
+        trending_repo_info["name"]
+        for trending_repo_info in trending_repos_info_list
+    ]
+    headers = {"Accept": "Application/JSON"}
+    uri = (
+        "https://api.github.com/search/issues?q=repo:{}+state:open".format(
+            "+repo:".join(trending_repos_names)
+        )
+    )
+    open_issues_common_responce = requests.get(uri, headers=headers)
+    open_issues_common_list = open_issues_common_responce.json()["items"]
+    for trending_repo_info in trending_repos_info_list:
+        open_issues = []
+        for open_issue in open_issues_common_list:
+            if trending_repo_info["url"] == open_issue["repository_url"]:
+                open_issues.append(
+                    {
+                        "title": open_issue["title"],
+                        "url": open_issue["url"]
+                    }
+                )
+        trending_repo_info["open_issues"] = open_issues
+    return trending_repos_info_list
+
+
+def print_pretty_info(trending_repos_info_list):
+    print(
+        "\n"
+        "Список 20  самых популярных репозиториев открыты["
+        "за последние 7 дней"
+    )
+    for trending_repo_info in trending_repos_info_list:
+        repo_name = trending_repo_info["name"]
+        repo_url = trending_repo_info["url"]
+        open_issues_list = trending_repo_info["open_issues"]
+        print(
+            "\n"
+            "--------------------------------------------------------------"
+            "\n"
+            "\n"
+            "Репозиторий: {},"
+            "\n"
+            "расположен {}".format(repo_name, repo_url)
+        )
+        if open_issues_list:
+            print(
+                "\n"
+                "открытые issues:"
+            )
+            for issue in open_issues_list:
+                print(
+                    "issue: {}"
+                    "\n"
+                    "описан {}".format(issue["title"], issue["url"])
+                )
+    print(
+        "\n"
+        "--------------------------------------------------------------"
+    )
+
 
 if __name__ == '__main__':
-    pass
+    days = 7
+    top_size = 20
+    reference_date = calculate_reference_date(days)
+    trending_repos_list = get_trending_repositories(top_size, reference_date)
+    trending_repos_info_list = get_open_issues(trending_repos_list)
+    print_pretty_info(trending_repos_info_list)
